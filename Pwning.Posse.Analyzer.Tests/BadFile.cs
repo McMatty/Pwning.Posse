fe8b536a4d3a49876dde4e02445efd1591857495
McMatty <McMatty@users.noreply.github.com>
2020-01-10 14:03:28 +1300

    A bad file to try trigger LGTM

---
+++ Pwning.Posse.Analyzer.Tests/BadFile.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json;
+namespace ConsoleApplication1
+{
+    class TypeName
+    {  
+        static void  Main(string[] args)
+        {
+            GetDeserializedObject(""payload"");
+        }
+        public static void GetDeserializedObject(string payload)
+        {
+                var obj = JsonConvert.DeserializeObject<Object>(payload, new JsonSerializerSettings
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto
+                    }); 
+        }
+    }
+}
