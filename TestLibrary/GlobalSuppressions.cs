fe564889c39c183b13347de8b94a285920f08319
37254facea80fdf094f8301c8ddfc3ee715577df <MEvans@fraedom.com>
2020-01-20 16:24:08 +1300

    test project with working security analzyer

---
+++ TestLibrary/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1822:Member TestMethod1 does not access instance data and can be marked as static (Shared in VisualBasic)", Justification = "<Pending>", Scope = "member", Target = "~M:Pwning.Posse.Common.Tests.StaticAnalysisUtilitesTests.TestMethod1")]
