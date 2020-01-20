20e8d9052b0f8d6c3293b13023540d270cb75d0a
37254facea80fdf094f8301c8ddfc3ee715577df <MEvans@fraedom.com>
2020-01-20 15:14:51 +1300

    Created test project for roslyn sonarqube checks

---
+++ TestLibrary/StaticAnalysisUtilitesTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Pwning.Posse.Common.Tests
+{
+    public class StaticAnalysisUtilitesTests
+    {
+        public void TestMethod1()
+        {
+            var credentials = new NetworkCredential("noreply@pumasecurity.io", "supersecretpassword");
+            _ = credentials.UserName;
+        }
+    }
+}
